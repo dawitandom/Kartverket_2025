25ca90d95e622ad4cf158be6ea6ef3838e6971cb
Dawit Ghirmay Andom <dawitga@uia.no>
2025-09-25 14:13:02 +0200

    oppgave 4 er klar gjort

---
+++ FirstWebApplication/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using FirstWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    public class ReportController : Controller
+    {
+        [HttpGet]
+        public IActionResult Scheme()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Scheme(ReportDTO reportDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return RedirectToAction("FormValid", reportDto);
+        }
+        
+        
+          
+
+            
+        public IActionResult FormValid(ReportDTO schemeValues)
+
+        {
+            return View(schemeValues);
+        }
+    }
+}
