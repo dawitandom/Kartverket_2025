fbc3636b2e205018c686ea666c284e8c4048df5a
dawitandom <dawitandom1409@gmail.com>
2025-10-01 13:26:00 +0200

    Dokumentasjon i Migrations

--- FirstWebApplication/Migrations/20250916104717_InitialMigration.cs
+++ FirstWebApplication/Migrations/20250916104717_InitialMigration.cs
@@ -5,36 +5,45 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace FirstWebApplication.Migrations
 {
-    /// <inheritdoc />
+    // Dette er en migrasjon.Den brukes til å oppretter eller endre tabeller i databasen
     public partial class InitialMigration : Migration
     {
-        /// <inheritdoc />
+        // Denne metoden kjøres når migrasjonen blir lagt til (lager tabell)
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // setter database til å bruke utf8mb4
             migrationBuilder.AlterDatabase()
                 .Annotation("MySql:CharSet", "utf8mb4");
-
+            
+            // Lager en tabell som heter "Advices"
             migrationBuilder.CreateTable(
                 name: "Advices",
                 columns: table => new
                 {
+                    // Kolonnen Title, tekst som må fylles ut
                     AdviceId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+
+                    // Kolonnen Title, tekst som må fylles ut
                     Title = table.Column<string>(type: "longtext", nullable: false)
                         .Annotation("MySql:CharSet", "utf8mb4"),
+
+                    // Kolonnen Description, tekst som må fylles ut
                     Description = table.Column<string>(type: "longtext", nullable: false)
                         .Annotation("MySql:CharSet", "utf8mb4")
                 },
                 constraints: table =>
                 {
+                    // Setter AdviceId som primærnøkkel
                     table.PrimaryKey("PK_Advices", x => x.AdviceId);
                 })
                 .Annotation("MySql:CharSet", "utf8mb4");
         }
 
-        /// <inheritdoc />
+        // Dene metoden kjøres hvis man ruller migrasjonen tilbake (sletter tabell)
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            // Sletter tabellen "Advices"
             migrationBuilder.DropTable(
                 name: "Advices");
         }
