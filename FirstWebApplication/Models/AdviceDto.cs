2ee7916defe4618948451febd9121c2ef7c1c55d
MariusKhiem <mariuskn@uia.no>
2025-10-03 20:14:30 +0200

    kommentering av koder

--- FirstWebApplication/Models/AdviceDto.cs
+++ FirstWebApplication/Models/AdviceDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace FirstWebApplication.Models
 {
-    public class AdviceDto
+    public class AdviceDto //Gir default verdier (advice = 0, string = null, description null)
     {
         public int AdviceId { get; set; }
         public string Title { get; set; }
