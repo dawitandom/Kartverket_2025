86d1dbce4377b9736ab357e48f478875f6590b27
haakonhalvors1 <haakonhalv04@gmail.com>
2025-10-22 12:48:16 +0200

    Admin kan lage og slette brukere

---
+++ FirstWebApplication/Models/ViewModel/UserManagementViewModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace FirstWebApplication.Models;
+
+public class UserManagementViewModel
+{
+    public string Id { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public List<string> Roles { get; set; } = new List<string>();
+}
