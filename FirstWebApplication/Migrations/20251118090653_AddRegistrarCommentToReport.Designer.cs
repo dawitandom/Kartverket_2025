92d5709d7ad71eb5d6eb77b4dfb09dad748df21a
haakonhalvors1 <haakonhalv04@gmail.com>
2025-11-18 10:14:10 +0100

    Lagt inn registrar Commentar på rapportene
    Co-Authored-By: dawitandom <179924332+dawitandom@users.noreply.github.com>

--- FirstWebApplication/Migrations/20251118090653_AddRegistrarCommentToReport.Designer.cs
+++ FirstWebApplication/Migrations/20251118090653_AddRegistrarCommentToReport.Designer.cs
@@ -4,6 +4,7 @@ using FirstWebApplication.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FirstWebApplication.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20251118090653_AddRegistrarCommentToReport")]
+    partial class AddRegistrarCommentToReport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
